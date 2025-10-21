4bc0f3ff2d89b0c4b50fbb35086eeb505084d7e5
Sharon Njoroge <waithirasharon222@gmail>
2025-10-21 15:48:04 +0300

    add work

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  let emoji = "";
+  if (minutes < 30) {
+    const cups = Math.ceil(minutes / 5);
+    emoji = "☕️".repeat(cups);
+  } else {
+    const bentos = Math.ceil(minutes / 10);
+    emoji = "🍱".repeat(bentos);
+  }
+
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date}</small>
+      <small>
+        {date} • {emoji} {minutes} min read
+      </small>
       <p>{preview}</p>
     </article>
   );
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,19 +2,19 @@ import React from "react";
 import Article from "./Article";
 
 function ArticleList({ posts }) {
-  return (
-    <main>
-      {posts.map((post) => (
-        <Article
-          key={post.id}
-          title={post.title}
-          date={post.date}
-          preview={post.preview}
-          minutes={post.minutes}
-        />
-      ))}
-    </main>
-  );
+  const articleElements = posts.map((post) => {
+    return (
+      <Article
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+        minutes={post.minutes}
+      />
+    );
+  });
+
+  return <main>{articleElements}</main>;
 }
 
 export default ArticleList;
